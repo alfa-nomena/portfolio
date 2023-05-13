2591f45521b9911a3fb965e14861467f35ea8a62
Nomena Alfa ANDRIAMIFIDISOA <perf.alfa@mmv.madamvalue.mg>
2023-05-13 15:11:05 +0300

    Models created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Owner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    short_description = models.TextField(null=True)
+    description = models.TextField(null=True)
+    image = models.ImageField(upload_to="about", default="moi.png", null=True)
+    name = lambda self: f"{self.user.first_name} {self.user.last_name}"
+    cv = models.FileField("Curriculum Vitae", upload_to="owner", blank=True, null=True,)
+    class Meta:
+        verbose_name = "About me"
+        verbose_name_plural = "About me"
+        
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    duration = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to="skill", null=True)
+    
+    def __str__(self):
+        return self.name
+
+class SkillDetail(models.Model):
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    level = models.IntegerField(
+        default=50, 
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
+    
+class Qualification(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='qualification', null=True)
+
+class QualificationDetail(models.Model):
+    qualification = models.ForeignKey(Qualification, on_delete=models.CASCADE)
+    start = models.DateField()
+    end = models.DateField(default=timezone.now)
+    name = models.CharField(max_length=100)
+    detail = models.CharField(max_length=100)
+        
+class Service(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField("Service name",max_length=100)
+    logo = models.ImageField(upload_to="services", null=True)
+    
+    def __str__(self):
+        return self.name
+
+class ServiceDetail(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    description = models.CharField("Service name",max_length=100)
+    
+class Portfolio(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    link = models.URLField()
+    description = models.TextField()
+    
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
-
+from typing import Any
+from django import http
+from django.views.generic import TemplateView
+from .models import *
+from faker import Faker
+import numpy as np
 # Create your views here.
+
+class HomeTemplateView(TemplateView):
+    template_name = "base/home.html"
+    def get(self, request, *args, **kwargs):
+        owners = Owner.objects.all()
+        if not owners:
+            
+            users = User.objects.all()
+            if users:
+                user=users[0]
+            else:
+                user = User.objects.create_user(username='test', password='test', email="hackibosy@gmail.com")
+                user.is_superuser = True
+                user.is_staff = True
+                user = user.save()
+            faker = Faker()
+            owner = Owner.objects.create(
+                user=user,
+                short_description = faker.text(100),
+                description=faker.text(200),
+            )
+            owner = owner.save()
+            
+            for _ in range(2):
+                skill = Skill.objects.create(
+                    owner=owner,
+                    name = faker.text(100),
+                    duration = faker.text(100)
+                )
+                skill = skill.save()
+                for _ in range(5):
+                    skill_detail = SkillDetail.objects.create(
+                        skill=skill,
+                        level = np.random.randint(1,101)
+                    )
+                    skill_detail.save()
+            
+            for _ in range(2):
+                qual = Qualification.objects.create(
+                    owner=owner,
+                    name = faker.text(100),
+                )
+                qual = qual.save()
+                for _ in range(5):
+                    qual_detail = QualificationDetail.objects.create(
+                        qualification=qual,
+                        start=timezone.now(),
+                        end=timezone.now(),
+                        name=faker.text(30),
+                        detail=faker.text(100)
+                    )
+                    qual_detail.save()
+                    
+            for _ in range(2):
+                serv = Service.objects.create(
+                    owner=owner,
+                    name = faker.text(100),
+                )
+                serv = serv.save()
+                for _ in range(5):
+                    serv_detail = ServiceDetail.objects.create(
+                        service=serv,
+                        description=faker.text(100)
+                    )
+                    qual_detail.save()
+            
+            for _ in range(3):
+                port = Portfolio.objects.create(
+                    owner=owner,
+                    name = faker.text(30),
+                    link = faker.url(),
+                    description = faker.text(100)
+                )
+                port.save()
+
+
+
+
+
+
+
+
+
+
+        return super().get(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["owner"] = Owner.objects.get(pk=1)
+        context['services'] = Service.objects.all()[:3]
+        # context["softs"] = SoftSkill.objects.filter(owner=context["owner"])
+        # context['works'] = Work.objects.all()[:2]
+        
+        
+        return context
+    
