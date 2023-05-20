6056a50a871991479958719cab4fa12ec822fe7d
Nomena Alfa ANDRIAMIFIDISOA <perf.alfa@mmv.madamvalue.mg>
2023-05-20 10:07:04 +0300

    --amend

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=isp$x8mx_-5u1w3skd=^@npfekx&hj*+-lh7&6*xa!g_p=c1z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  False
+DEBUG =  True
 
 
 ALLOWED_HOSTS = ['hackibosy.pythonanywhere.com', "127.0.0.1"]
